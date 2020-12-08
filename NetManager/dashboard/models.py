990359a50898be4efc601c4c896a20176c133fda
James White <james.white-12@students.plymouth.ac.uk>
2020-12-08 17:37:50 +0000

    Netmiko Testing

--- NetManager/dashboard/models.py
+++ NetManager/dashboard/models.py
@@ -5,20 +5,10 @@ from django.db import models
 class Device(models.Model):
     device = models.CharField(max_length=250)
     deviceType = models.CharField(max_length=250)
+    host = models.CharField(max_length=250)
+    username = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
     vendor = models.CharField(max_length=250)
-    interfaces = models.IntegerField()
 
     def __str__(self):
         return 'Device: {}'.format(self.device)
-
-
-# interface object
-class Interface(models.Model):
-    device = models.ForeignKey(Device, on_delete=models.CASCADE)
-    interface = models.CharField(max_length=250)
-    ipAddress = models.CharField(max_length=250)
-    subnet = models.CharField(max_length=250)
-    status = models.CharField(max_length=250)
-
-    def __str__(self):
-        return 'Interface: {}'.format(self.interface)
--- NetManager/dashboard/views.py
+++ NetManager/dashboard/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
 from .models import *
+from subprocess import run, PIPE
+import sys
 
 
-# Create your views here.
 def index(request):
     devices = Device.objects.all()
-    interfaces = Interface.objects.all()
-    args = {'devices': devices, 'interfaces': interfaces}
+    args = {'devices': devices}
     return render(request, 'index.html', args)
+
+
+
+def connect(request):
+    from netmiko import ConnectHandler
+    from datetime import datetime
+
+    # pass in device details
+    host = sys.argv[1]
+    username = sys.argv[2]
+    password = sys.argv[3]
+
+    # setup ssh connection
+    net_connect = ConnectHandler(
+        device_type='cisco_ios',
+        host='192.168.68.10',
+        username='admin',
+        password='cisco',
+        port='22',
+        secret='cisco'
+    )
+
+    command = 'show ip int brief'
+    output = net_connect.send_command(command)
+
+    return render(request, 'index.html', {'details': output})
